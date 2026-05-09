6bdc5d5fdd6886d9d4643bc2ff9dcd485f70f7d7
Pascal Ehlert <pascal@ehlert-it.de>
2026-05-09 13:40:23 +0200

    Replace deprecated scheme.Builder with local equivalent
    Inlines a small Builder in api/v1beta1 so the package depends only on
    k8s.io/apimachinery, fixing the SA1019 staticcheck warning.

--- api/v1beta1/groupversion_info.go
+++ api/v1beta1/groupversion_info.go
@@ -4,8 +4,9 @@
 package v1beta1
 
 import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
 
 var (
@@ -13,8 +14,37 @@ var (
 	GroupVersion = schema.GroupVersion{Group: "keycloak.hostzero.com", Version: "v1beta1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
-	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+	SchemeBuilder = &Builder{GroupVersion: GroupVersion}
 
 	// AddToScheme adds the types in this group-version to the given scheme.
 	AddToScheme = SchemeBuilder.AddToScheme
 )
+
+// Builder builds a new Scheme for mapping go types to Kubernetes GroupVersionKinds.
+//
+// This is a local replacement for the deprecated
+// sigs.k8s.io/controller-runtime/pkg/scheme.Builder, kept here so that this api
+// package depends only on k8s.io/apimachinery. The behaviour is intentionally
+// identical to the upstream Builder.
+type Builder struct {
+	GroupVersion schema.GroupVersion
+	runtime.SchemeBuilder
+}
+
+// Register adds the given objects to the SchemeBuilder so they can be added
+// to a Scheme. Returns the Builder for chaining.
+func (b *Builder) Register(objects ...runtime.Object) *Builder {
+	b.SchemeBuilder.Register(func(scheme *runtime.Scheme) error {
+		for _, obj := range objects {
+			scheme.AddKnownTypes(b.GroupVersion, obj)
+		}
+		metav1.AddToGroupVersion(scheme, b.GroupVersion)
+		return nil
+	})
+	return b
+}
+
+// AddToScheme adds all the registered types to the given Scheme.
+func (b *Builder) AddToScheme(s *runtime.Scheme) error {
+	return b.SchemeBuilder.AddToScheme(s)
+}
